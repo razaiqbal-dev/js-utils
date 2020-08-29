47366722324b8c4d3b4ef90938d017c1f4035a6b
unknown <mailrazaiqbal@gmail.com>
2020-08-29 12:25:17 +0530

    array sort

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
 const checkSimilarity = require("../index");
+const arraySort = require("../index");
 
 console.log(checkSimilarity("test phrase", ["test phrase 1"]));
+console.log(arraySort([4, 5, 6, 2, 5, 7, 2, 0, 3]));
